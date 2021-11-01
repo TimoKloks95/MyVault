6de88bb7f264bcbe31fa8c29bb8edf26ea99a20c
Timo95 <timokloks@hotmail.com>
2021-11-01 15:30:35 +0100

    Spring boot project gegenereert

---
+++ src/main/java/nl/timokloks/myvault/MyvaultApplication.java
@@ -0,0 +1,13 @@
+package nl.timokloks.myvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/timokloks/myvault/MyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.timokloks.myvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
